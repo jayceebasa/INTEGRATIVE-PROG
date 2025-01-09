e381eb19dc911c5361ffea8f9c547dad79ffeb5d
Juan Carlos Miguel T. Basa <143696331+jayceebasa@users.noreply.github.com>
2025-01-10 03:39:05 +0800

    asdf

--- connectly_project/connectly_project/urls.py
+++ connectly_project/connectly_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from connectly_project.views import homepage
+
 
 urlpatterns = [
+    path('', homepage, name="home"),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logouts
 ]
+
+
+
