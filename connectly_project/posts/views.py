809e81cbcd5ca21bc2ff2910e12c9d9b09a38140
Juan Carlos Miguel T. Basa <143696331+jayceebasa@users.noreply.github.com>
2025-02-03 14:53:26 +0800

    complete crud operations

--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -4,13 +4,11 @@ from rest_framework import status
 from .models import User, Post, Comment
 from .serializers import UserSerializer, PostSerializer, CommentSerializer
 
-
 class UserListCreate(APIView):
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -19,6 +17,18 @@ class UserListCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request):
+        user = User.objects.get(id=request.data['id'])
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        user = User.objects.get(id=request.data['id'])
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PostListCreate(APIView):
     def get(self, request):
@@ -34,7 +44,18 @@ class PostListCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request):
+        post = Post.objects.get(id=request.data['id'])
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request):
+        post = Post.objects.get(id=request.data['id'])
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 class CommentListCreate(APIView):
     def get(self, request):
         comments = Comment.objects.all()
@@ -49,3 +70,15 @@ class CommentListCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request):
+        comment = Comment.objects.get(id=request.data['id'])
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+    def delete(self, request):  
+        comment = Comment.objects.get(id=request.data['id'])
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
